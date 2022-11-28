46105771b223ef199664d26c7d5f3ec5b7658112
erdodo <erdoganyesil3@gmail.com>
2022-11-28 15:39:36 +0300

    blog detay yorumlar ve butonlar

--- pages/detay/_title.vue
+++ pages/detay/_title.vue
@@ -11,93 +11,199 @@
               <el-breadcrumb-item> {{ $route.params.title.replace("-", " ") }} </el-breadcrumb-item>
             </el-breadcrumb>
           </el-card>
-          <div v-if="blog_detail.video_link != null">
-            {{ blog_detail.video_link }}
-          </div>
-          <div class="d-flex justify-content-center">
-            <el-image :src="getBlogImage(blog_detail.image)" style="width: 1000px"></el-image>
-          </div>
-          <h1 class="text-center w-100 my-3">
-            {{ blog_detail.title }}
-          </h1>
-          <div class="my-3 d-flex justify-content-end">
-            <el-tag class="m-1" v-for="ktg in blog_kategoriler" :key="ktg"> {{ ktg.display }} </el-tag>
-          </div>
-          <div class="container my-3 p-0 px-sm-1 px-md-2 px-lg-3 px-xl-4 px-xxl-5">
-            <div v-html="blog_detail.html_content"></div>
-          </div>
-          <div class="d-flex justify-content-between my-3 fs-7 text-info">
-            <div class="d-flex">
-              <a class="btn btn-secondary rounded-circle pt-2 mx-1 cursor-pointer"
-                ><i class="bi m-0 fs-5 bi-bookmark"></i
-              ></a>
-              <a class="btn btn-secondary rounded-circle pt-2 mx-1 cursor-pointer"
-                ><i class="bi m-0 fs-5 bi-hand-thumbs-up"></i
-              ></a>
-              <a
-                class="btn rounded-circle pt-2 btn-secondary mx-1"
-                :href="'https://www.facebook.com/sharer/sharer.php?u=' + location_href"
-                target="_blank"
-              >
-                <i class="bi m-0 fs-5 bi-facebook"></i>
-              </a>
-              <a
-                class="btn rounded-circle pt-2 btn-secondary mx-1"
-                :href="'whatsapp://send?text=' + location_href"
-                data-action="share/whatsapp/share"
-                ><i class="bi m-0 fs-5 bi-whatsapp"></i
-              ></a>
-              <a
-                class="btn rounded-circle pt-2 btn-secondary mx-1"
-                :href="'https://twitter.com/intent/tweet?text=' + location_href"
-                target="_blank"
-                ><i class="bi m-0 fs-5 bi-twitter"></i
-              ></a>
-              <a
-                class="btn rounded-circle pt-2 btn-secondary mx-1"
-                :href="'https://www.linkedin.com/sharing/share-offsite/?url=' + location_href"
-                target="_blank"
-                ><i class="bi m-0 fs-5 bi-linkedin"></i
-              ></a>
-            </div>
-            <label for="">{{ moment(blog_detail.created_at).format("Do MMMM  YYYY, h:mm") }}</label>
-          </div>
-          <el-card class="my-4">
-            <div class="d-flex">
-              <el-image
-                :src="getBlogImage(user_detail.profile_picture)"
-                class="rounded-circle"
-                style="height: 100px; width: 100px; object-fit: contain"
-              ></el-image>
-              <div class="px-4">
-                <h5>{{ user_detail.name_basic }} {{ user_detail.surname }}</h5>
-                <p>{{ user_detail.description }}</p>
+
+          <div class="row">
+            <div class="col-12 col-sm-11">
+              <div v-if="blog_detail.video_link != null" class="d-flex justify-content-center">
+                <iframe
+                  width="1000px"
+                  height="500px"
+                  :src="blog_detail.video_link.replace('watch?v=', 'embed/').split('&t')[0]"
+                  :title="blog_detail.title"
+                  frameborder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                ></iframe>
               </div>
-            </div>
-          </el-card>
+              <div v-else class="d-flex justify-content-center">
+                <el-image :src="getBlogImage(blog_detail.image)" :alt="blog_detail.title" style="width: 1000px"></el-image>
+              </div>
+              <h1 class="text-center w-100 my-3">
+                {{ blog_detail.title }}
+              </h1>
 
-          <div class="my-4">
-            <h3>Diğer Bloglarımız</h3>
-            <el-divider></el-divider>
-            <el-carousel :interval="4000" type="card" height="200px">
-              <el-carousel-item v-for="blog in blogs" :key="blog.title">
-                <div class="position-relative card">
-                  <el-image :src="getBlogImage(blog.image)" fit="cover" class="w-100" style="height: 200px">
-                    <div slot="placeholder" class="image-slot">Loading<span class="dot">...</span></div>
-                  </el-image>
+              <div class="container my-3 p-0 px-sm-1 px-md-2 px-lg-3 px-xl-4 px-xxl-5">
+                <div v-html="blog_detail.html_content"></div>
+              </div>
+              <div class="d-flex align-items-center justify-content-between">
+                <div class="my-3 d-flex">
+                  <el-tag class="m-1" v-for="ktg in blog_kategoriler" :key="ktg"> {{ ktg.display }} </el-tag>
+                </div>
+                <label for="">{{ moment(blog_detail.created_at).format("Do MMMM  YYYY, h:mm") }}</label>
+              </div>
 
-                  <div
-                    class="position-absolute flex-column text-center top-0 w-100 h-100 d-flex justify-content-center align-items-center"
-                    style="background: rgba(0, 0, 0, 0.7)"
+              <div class="d-flex d-sm-none">
+                <div>
+                  <a class="btn btn-secondary rounded-circle pt-2 mx-1 cursor-pointer"
+                    ><i class="bi m-0 fs-5 bi-bookmark"></i
+                  ></a>
+                </div>
+                <div>
+                  <a class="btn btn-secondary rounded-circle pt-2 mx-1 cursor-pointer"
+                    ><i class="bi m-0 fs-5 bi-hand-thumbs-up"></i
+                  ></a>
+                </div>
+                <div>
+                  <a
+                    class="btn rounded-circle pt-2 btn-secondary mx-1"
+                    :href="'https://www.facebook.com/sharer/sharer.php?u=' + location_href"
+                    target="_blank"
                   >
-                    <h4 class="text-white">{{ blog.title }}</h4>
-                    <a :href="'/detay/' + blog.title.replace(/\s/g, '-') + '?b=' + blog.id" class="text-white">Devamı...</a>
+                    <i class="bi m-0 fs-5 bi-facebook"></i>
+                  </a>
+                </div>
+                <div>
+                  <a
+                    class="btn rounded-circle pt-2 btn-secondary mx-1"
+                    :href="'whatsapp://send?text=' + location_href"
+                    data-action="share/whatsapp/share"
+                    ><i class="bi m-0 fs-5 bi-whatsapp"></i
+                  ></a>
+                </div>
+                <div>
+                  <a
+                    class="btn rounded-circle pt-2 btn-secondary mx-1"
+                    :href="'https://twitter.com/intent/tweet?text=' + location_href"
+                    target="_blank"
+                    ><i class="bi m-0 fs-5 bi-twitter"></i
+                  ></a>
+                </div>
+                <div>
+                  <a
+                    class="btn rounded-circle pt-2 btn-secondary mx-1"
+                    :href="'https://www.linkedin.com/sharing/share-offsite/?url=' + location_href"
+                    target="_blank"
+                    ><i class="bi m-0 fs-5 bi-linkedin"></i
+                  ></a>
+                </div>
+              </div>
+              <el-card class="my-4">
+                <div class="d-flex">
+                  <el-image
+                    :src="getBlogImage(user_detail.profile_picture)"
+                    class="rounded-circle"
+                    style="height: 100px; width: 100px; object-fit: contain"
+                  ></el-image>
+                  <div class="px-4">
+                    <h5>{{ user_detail.name_basic }} {{ user_detail.surname }}</h5>
+                    <p>{{ user_detail.description }}</p>
                   </div>
                 </div>
-              </el-carousel-item>
-            </el-carousel>
+              </el-card>
+
+              <div class="my-4">
+                <h3>Diğer Bloglarımız</h3>
+                <el-divider></el-divider>
+                <el-carousel :interval="4000" type="card" height="200px">
+                  <el-carousel-item v-for="blog in blogs" :key="blog.title">
+                    <div class="position-relative card">
+                      <el-image :src="getBlogImage(blog.image)" fit="cover" class="w-100" style="height: 200px">
+                        <div slot="placeholder" class="image-slot">Loading<span class="dot">...</span></div>
+                      </el-image>
+
+                      <div
+                        class="position-absolute flex-column text-center top-0 w-100 h-100 d-flex justify-content-center align-items-center"
+                        style="background: rgba(0, 0, 0, 0.7)"
+                      >
+                        <h4 class="text-white">{{ blog.title }}</h4>
+                        <a :href="'/detay/' + blog.title.replace(/\s/g, '-') + '?b=' + blog.id" class="text-white"
+                          >Devamı...</a
+                        >
+                      </div>
+                    </div>
+                  </el-carousel-item>
+                </el-carousel>
+              </div>
+              <div class="my-5"></div>
+            </div>
+            <div class="d-none d-sm-block col-sm-1 position-relative">
+              <div class="d-flex flex-column position-sticky" style="top: calc(50vh - 162px)">
+                <div>
+                  <a class="btn btn-secondary rounded-circle pt-2 my-1 cursor-pointer"
+                    ><i class="bi m-0 fs-5 bi-bookmark"></i
+                  ></a>
+                </div>
+                <div>
+                  <a class="btn btn-secondary rounded-circle pt-2 my-1 cursor-pointer"
+                    ><i class="bi m-0 fs-5 bi-hand-thumbs-up"></i
+                  ></a>
+                </div>
+                <div>
+                  <a
+                    class="btn rounded-circle pt-2 btn-secondary my-1"
+                    :href="'https://www.facebook.com/sharer/sharer.php?u=' + location_href"
+                    target="_blank"
+                  >
+                    <i class="bi m-0 fs-5 bi-facebook"></i>
+                  </a>
+                </div>
+                <div>
+                  <a
+                    class="btn rounded-circle pt-2 btn-secondary my-1"
+                    :href="'whatsapp://send?text=' + location_href"
+                    data-action="share/whatsapp/share"
+                    ><i class="bi m-0 fs-5 bi-whatsapp"></i
+                  ></a>
+                </div>
+                <div>
+                  <a
+                    class="btn rounded-circle pt-2 btn-secondary my-1"
+                    :href="'https://twitter.com/intent/tweet?text=' + location_href"
+                    target="_blank"
+                    ><i class="bi m-0 fs-5 bi-twitter"></i
+                  ></a>
+                </div>
+                <div>
+                  <a
+                    class="btn rounded-circle pt-2 btn-secondary my-1"
+                    :href="'https://www.linkedin.com/sharing/share-offsite/?url=' + location_href"
+                    target="_blank"
+                    ><i class="bi m-0 fs-5 bi-linkedin"></i
+                  ></a>
+                </div>
+              </div>
+            </div>
           </div>
-          <div class="my-5"></div>
+          <el-divider></el-divider>
+          <div class="d-flex w-100 flex-column">
+            <span>Yorumunuz:</span>
+            <el-input type="textarea" :rows="3" placeholder="Yorumunuzu buraya yazabilirsiniz." v-model="textarea">
+            </el-input>
+            <div class="d-flex justify-content-end">
+              <el-button type="primary" class="my-2">Gönder</el-button>
+            </div>
+            <div class="my-5">
+              <el-timeline>
+                <el-timeline-item timestamp="2018/4/12" placement="top">
+                  <el-card>
+                    <h4>Update Github template</h4>
+                    <p>Tom committed 2018/4/12 20:46</p>
+                  </el-card>
+                </el-timeline-item>
+                <el-timeline-item timestamp="2018/4/3" placement="top">
+                  <el-card>
+                    <h4>Update Github template</h4>
+                    <p>Tom committed 2018/4/3 20:46</p>
+                  </el-card>
+                </el-timeline-item>
+                <el-timeline-item timestamp="2018/4/2" placement="top">
+                  <el-card>
+                    <h4>Update Github template</h4>
+                    <p>Tom committed 2018/4/2 20:46</p>
+                  </el-card>
+                </el-timeline-item>
+              </el-timeline>
+            </div>
+          </div>
         </div>
       </div>
     </div>
